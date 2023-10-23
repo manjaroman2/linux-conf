47f7d7809f4a4f1ec6d90132229b4e1f165ed93a
manjaroman <manjaroman@protonmail.com>
2023-10-23 09:57:33 +0200

    space

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-python -c "from common import init_state; init_state();" 
-source ~/.bashrc # We assume you have bin_path in your $PATH, you do have it there, right? 
+python -c "from common import init_state; init_state();"
+source ~/.bashrc # We assume you have bin_path in your $PATH, you do have it there, right?
