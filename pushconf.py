54a08e2894d0acdc6a049da1ed3b5a22434a99fc
manjaroman2 <manjaroman2@protonmail.com>
2023-10-26 15:47:50 +0200

    pushconf

--- pushconf.py
+++ pushconf.py
@@ -73,7 +73,7 @@ def make_tarfile(output_filename, source_dir: Path, compression="xz"):
             return self.fillchar*self.level
     def filter_func(info: tarfile.TarInfo, t: T):
         this_path = Path(info.name)
-        print(this_path.upper())
+        print(this_path.as_posix().upper())
         if info.isdir():
             if t.curr_dir:
                 # print(t.curr_dir, list(this_path.parents))
