eef9e254d7ae0605f11656ec8e2e7854fe163f08
manjaroman2 <manjaroman2@protonmail.com>
2023-10-26 15:33:18 +0200

    pushconf

--- pushconf.py
+++ pushconf.py
@@ -69,9 +69,10 @@ def make_tarfile(output_filename, source_dir: Path, compression="xz"):
     def filter_func(info: tarfile.TarInfo, t: T):
         if info.isdir():
             this_path = Path(info.name)
-            print(t.curr_dir, this_path.parents)
-            if t.curr_dir not in this_path.parents: # Not subdir
-                t.level = t.curr_dir.parts.index(this_path.parent)
+            if t.curr_dir:
+                print(t.curr_dir, this_path.parents)
+                if t.curr_dir not in this_path.parents: # Not subdir
+                    t.level = t.curr_dir.parts.index(this_path.parent)
             t.curr_dir = this_path 
             print("--"* t.level + "📁 " + t.curr_dir.parts[t.level])
             t.level += 1
