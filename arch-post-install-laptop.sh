3b7a512303566a805fef140fd7872fa5d04710e3
manjaroman <manjaroman@protonmail.com>
2023-10-29 22:31:29 +0100

    pushconf

--- arch-post-install-laptop.sh
+++ arch-post-install-laptop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-sudo pacman -S tlp powertop alacritty ttf-fantasque-nerd python-scipy python-ipympl python-numpy python-matplotlib
+sudo pacman -S tlp powertop alacritty ttf-fantasque-nerd python-scipy python-ipympl python-numpy python-matplotlib python-sympy
